9a3de582dd66defce394b0456fb846bc7f367649
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-11-11 07:42:46 +0100

    in the frontend directory update files app.module and material.module (11/11/2023)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,64 +1,68 @@
 import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
+import {AppRoutingModule} from "./app-routing.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AppRoutingModule} from "./app-routing.module";
+import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {MaterialModule} from "./material.module";
-import {MainComponent} from "./components/main/main.component";
-import {NavbarComponent} from "./components/navbar/navbar.component";
-import {SidebarComponent} from "./components/sidebar/sidebar.component";
-import {CalculatorComponent} from "./components/calculator/calculator.component";
+
 import {BeerManagerComponent} from "./components/beermanager/beermanager.component";
+import {CalculatorComponent} from "./components/calculator/calculator.component";
 import {CardComponent} from "./components/card/card.component";
-import {ProfileComponent} from "./components/profile/profile.component";
 import {ChangePasswordComponent} from "./components/changepassword/changepassword.component";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
-import {LoginComponent} from "./components/login/login.component";
-import {RegisterComponent} from "./components/register/register.component";
 import {HomepageComponent} from "./components/homepage/homepage.component";
-import {UploadAvatarComponent} from "./components/uploadavatar/uploadavatar.component";
-import {ModalMessageComponent} from "./components/modalmessage/modalmessage.component";
+import {LoginComponent} from "./components/login/login.component";
+import {MainComponent} from "./components/main/main.component";
 import {ModalFormComponent} from "./components/modalform/modalform.component";
-import {UpdateAvatarImageService} from "./services/updateavatarimage.service";
+import {ModalMessageComponent} from "./components/modalmessage/modalmessage.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {UploadAvatarComponent} from "./components/uploadavatar/uploadavatar.component";
+
 import {AuthService} from "./services/auth.service";
-import {HttpRequestService} from "./services/httprequest.service";
 import {DialogManagerService} from "./services/dialogmanager.service";
+import {HttpRequestService} from "./services/httprequest.service";
+import {ResponsiveSidebarService} from "./services/responsivesidebar.service";
+import {UpdateAvatarImageService} from "./services/updateavatarimage.service";
 import {UpdateCardChangedService} from "./services/updatecardchanged.service";
 
 @NgModule({
   declarations:[
-    MainComponent,
-    NavbarComponent,
-    SidebarComponent,
-    CalculatorComponent,
     BeerManagerComponent,
+    CalculatorComponent,
     CardComponent,
-    ProfileComponent,
     ChangePasswordComponent,
     DashboardComponent,
-    LoginComponent,
-    RegisterComponent,
     HomepageComponent,
-    UploadAvatarComponent,
+    LoginComponent,
+    MainComponent,
+    ModalFormComponent,
     ModalMessageComponent,
-    ModalFormComponent
+    NavbarComponent,
+    SidebarComponent,
+    ProfileComponent,
+    RegisterComponent,
+    UploadAvatarComponent
   ],
   imports:[
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule,
     MaterialModule
   ],
   providers:[
     AuthService,
-    HttpRequestService,
-    UpdateAvatarImageService,
     DialogManagerService,
+    HttpRequestService,
+    ResponsiveSidebarService,
+    UpdateAvatarImageService,
     UpdateCardChangedService
   ],
   bootstrap:[MainComponent]
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,25 +1,16 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {MatDialogModule} from "@angular/material/dialog";
-import {MatInputModule} from "@angular/material/input";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   imports:[
     CommonModule,
     MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     MatIconModule
   ],
   exports:[
     MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     MatIconModule
   ]
 })
