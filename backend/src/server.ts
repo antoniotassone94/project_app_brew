5cb8fec06159645d5ec6a439345b8f6e8dc2fd97
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-11-11 20:19:20 +0100

    in the backend directory changed the message in the file server.ts (11/11/2023)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,5 +16,5 @@ server.use("/app",app);
 server.get("/",(req,res) => res.send("Hello world!"));
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/`);
+    console.log(`Server is running on the port ${PORT}`);
 });
