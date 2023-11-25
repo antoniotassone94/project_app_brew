aa0f120751f6791560ce46fbd825e5d4102cf350
antoniotassone94 <antonio.tassone@studio.unibo.it>
2023-11-25 17:37:50 +0100

    update guard for authentication (angular application)

--- frontend/src/app/services/auth.guard.ts
+++ frontend/src/app/services/auth.guard.ts
@@ -1,15 +1,11 @@
-import {CanActivateFn} from "@angular/router";
-import {CanActivateChildFn} from "@angular/router";
+import {inject} from "@angular/core";
+import {CanActivateFn,CanActivateChildFn} from "@angular/router";
 import {AuthService} from "./auth.service";
 
 export const authGuardParent:CanActivateFn = (route,state) => {
-  //const authService:AuthService = new AuthService();
-  //return authService.isLogged();
-  return true;
+  return inject(AuthService).isLogged();
 };
 
 export const authGuardChildren:CanActivateChildFn = (childRoute,state) => {
-  //const authService:AuthService = new AuthService();
-  //return authService.isLogged();
-  return true;
+  return inject(AuthService).isLogged();
 };
